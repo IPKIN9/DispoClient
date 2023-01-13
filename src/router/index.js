dd1fb962c5de016b6d8bfc5473711f26cb70d1dd
nzrkodi <nzrkodi@gmail.com>
2023-01-13 22:06:03 +0800

    Create page pengajuan

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
 import Staff from '../views/Staff.vue'
 import Mahasiswa from '../views/Mahasiswa.vue'
+import Pengajuan from '../views/Pengajuan.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/mahasiswa',
       name: 'mahasiswa',
       component: Mahasiswa
+    },
+    {
+      path: '/pengajuan',
+      name: 'pengajuan',
+      component: Pengajuan
     }
   ]
 })
---
+++ src/views/Pengajuan.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3><b>Form Pengajuan</b></h3>
+                </div>
+                <div class="card-body">
+                    <form action="" class="row">
+                       <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="form-label">Nama Lengkap</label>
+                                <BaseInput placeholder="Masukan disini..." />
+                            </div>	
+                            <div class="form-group">
+                                <label class="form-label">Stambuk</label>
+                                <BaseInput placeholder="Masukan disini..." />
+                            </div>
+                            <div class="form-group">
+                                <label class="form-label">Prodi</label>
+                                <BaseInput placeholder="Masukan disini..." />
+                            </div>
+                            <div class="form-group">
+                                <label class="form-label">Fakultas</label>
+                                <BaseInput placeholder="Masukan disini..." />
+                            </div>
+                       </div>
+                       <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="form-label">Keterangan</label>
+                                <textarea class="form-control" name="" id="" cols="30" rows="14">
+
+                                </textarea>
+                            </div>	
+                       </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+import BaseInput from '../components/Input/BaseInput.vue'
+</script>
