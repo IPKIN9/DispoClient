289e1b6c68f647eb41a83724acacebe46a1806bf
IPKIN9 <irwandirini@gmail.com>
2023-01-18 21:28:19 +0800

    Remove user

--- src/components/skelton/TopBar.vue
+++ src/components/skelton/TopBar.vue
@@ -3,7 +3,6 @@
 	<div class="logo-header" data-background-color="blue2">
 
 		<a href="index.html" class="logo">
-			<img :src="logoImage" alt="navbar brand" class="navbar-brand">
 		</a>
 		<button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse" data-target="collapse"
 			aria-expanded="false" aria-label="Toggle navigation">
